4f12f5d8f1c61a705555e1919c86e4ae56b80328
Thomas Sanjurjo <sanjurjo7@gmail.com>
2016-04-03 02:03:33 -0400

    Added journal functionality using internal list item.

--- program.py
+++ program.py
@@ -13,27 +13,32 @@ def run_event_loop():
     
     print('What do you want to do with your journal?')
     cmd = None
-
+    journal_data = []
 
     while cmd != 'x':
         cmd = input('[L]ist entries, [A]dd an entry, E[x]it: ')
+        clear_cmd = cmd.lower().strip()
 
-        if cmd.lower().strip() == 'l':
-            list_entries()
-        elif cmd.lower().strip() == 'a':
-            add_entry()
-        elif cmd.lower().strip() != 'x':
+        if clear_cmd == 'l':
+            list_entries(journal_data)
+        elif clear_cmd == 'a':
+            add_entry(journal_data)
+        elif clear_cmd != 'x':
             print('Sorry, we don\'t understand \'{}\'.'.format(cmd))
 
     print('Done, goodbye.')
 
 
-def list_entries():
-    print('Listing...')
+def list_entries(data):
+    print('Your journal entries:')
+    entries = reversed(data)
+    for idx, entry in enumerate(entries):
+        print('* [{}] {}'.format(idx+1, entry))
 
 
-def add_entry():
-    print('Adding...')
+def add_entry(data):
+    text = input('Type your entry, <enter> to exit: ')
+    data.append(text)
 
 
 main()
